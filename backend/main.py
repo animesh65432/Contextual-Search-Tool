0f92cee907a60c8975a41c794ff01de14b6be193
animesh65432 <kiranduttta234@gmail.com>
2025-10-05 14:16:59 +0530

    fix cors error

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="Contextual Search API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["http://localhost:5173","https://contextual-search-tool-wuc1.vercel.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
