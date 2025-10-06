8c11c087889e573d01e03600695f4c30193ae458
animesh65432 <kiranduttta234@gmail.com>
2025-10-06 12:30:22 +0530

    deploy to vercel

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,23 +1,37 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { StartupContext } from "@/context/StartupProvider"
 import { GetallStartup } from "@/api/startup"
 import type { StartupType } from "@/types"
 import SearchInput from '@/components/SearchInput'
 import StratupsTable from '@/components/StartupsTable'
+import { LoaderCircle, SplinePointer } from 'lucide-react'
 
 const HomePage: React.FC = () => {
     const { OnSelectStartups, Startups } = useContext(StartupContext)
+    const [Isloading, SetIsloading] = useState<boolean>(false)
 
     const fetch = async () => {
-        const response = await GetallStartup() as {
-            startups: StartupType[]
+        SetIsloading(true)
+        try {
+            const response = await GetallStartup() as {
+                startups: StartupType[]
+            }
+            OnSelectStartups(response.startups)
         }
-        OnSelectStartups(response.startups)
+        finally {
+            SetIsloading(false)
+        }
     }
 
     useEffect(() => {
         fetch()
     }, [])
+
+    if (Isloading) {
+        return <div className='flex h-dvh justify-center items-center'>
+            <LoaderCircle className='animate-spin' />
+        </div>
+    }
 
     if (!Startups || Startups.length == 0) {
         return null
